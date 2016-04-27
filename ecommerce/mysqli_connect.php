532ba07fcb7b3b349e896c80996c05b6c3fab77b
GunnarElof <gelofson@zagmail.gonzaga.edu>
2016-04-26 21:15:38 -0700

    Update mysqli_connect.php

--- ecommerce/mysqli_connect.php
+++ ecommerce/mysqli_connect.php
@@ -5,10 +5,10 @@
 //selects the database, and sets the encoding.
 
 //Set the database access information as constants: 
-DEFINE ('DB_USER', 'username'); 
-DEFINE ('DB_PASSWORD', 'password'); 
+DEFINE ('DB_USER', 'root'); 
+DEFINE ('DB_PASSWORD', 'usbw'); 
 DEFINE ('DB_HOST', 'localhost'); 
-DEFINE ('DB_NAME', 'sitename'); 
+DEFINE ('DB_NAME', 'ecommerce'); 
 
 //Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ("Could not connect to MySQL: ". mysqli_connect_error() ); 
